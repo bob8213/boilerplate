4cc0274930e9b2fddec5e95a9084bee443ab08f8
NewBobPlus <rschuhler@hotmail.fr>
2020-11-14 16:02:41 +0100

    parameters

--- README.md
+++ README.md
@@ -3,7 +3,12 @@ Copy a file into current directory.
 
 ## Usage
 - Put files in a 'files' directory where the executable is
-- run boilerplate with the filename as argument
+- Run boilerplate with the filename as argument
 ```
 boilerplate file.txt
+```
+
+- You can create parameters with %?%parameterName%?% in the source file
+```
+Hi ! My name is %?%name%?%
 ```
--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,23 @@ fn copy_file(arg: String) -> Result<(), Box<dyn std::error::Error>> {
         if path.is_dir() {
             return Err("Only files are currently supported".into());
         } else {
-            fs::copy(path, out)?;
+            let file = fs::read_to_string(&path)?;
+            let mut input = String::new();
+            let stdin = io::stdin();
+            let mut content = String::new();
+
+            for (i, s) in file.split("%?%").enumerate() {
+                content.push_str(if (i & 1) != 0 {
+                    println!("'{}':", s);
+                    input.clear();
+                    stdin.read_line(&mut input).unwrap();
+                    input.trim_end()
+                } else {
+                    s
+                });
+            }
+
+            fs::write(out, content)?;
         }
     } else {
         return Err(io::Error::new(io::ErrorKind::NotFound, path.to_str().unwrap()).into());
